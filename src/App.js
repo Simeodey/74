bc71be39d850f5217c44acc0bd7514a1dc6e355a
Simeodey <adamand12@abv.bg>
2022-12-10 23:08:08 +0200

    dasdasd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useCallback, useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const isNumber = useMemo(()=>{
+    return !isNaN(Number(input))
+  },[input])
+
+  const handleInput = useCallback((e)=>{
+    setInput(e.target.value)
+  },[])
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +18,10 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          onInput={handleInput}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          {input && (isNumber?<i className="fas fa-check" />:<i className="fas fa-times" />)}
         </span> 
       </div>
     </div>
